12d47bd2562c3186741bded099da9592158d5d36
Lily-96 <LILIYASAVITSKA@GMAIL.COM>
2025-05-30 16:06:35 +0200

    sono riuscita a riutilizzare i componenti mi viene da piangere 🥲

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -1,92 +1,21 @@
-import { useEffect, useState } from "react";
-import { Container, Row, Col, Card } from "react-bootstrap";
-import AlbumCard from "./AlbumCard";
+import { Container } from "react-bootstrap";
+import MusicSection from "./MusicSection";
 
-const MainSection = () => {
-  const [rock, setRock] = useState([]);
-  const [pop, setPop] = useState([]);
-  const [hiphop, setHipHop] = useState([]);
+const MainSection = () => (
+  <main className="mainPage">
+    <Container fluid>
+      <div className="mainLinks d-flex mb-4">
+        <a href="#">TRENDING</a>
+        <a href="#">PODCAST</a>
+        <a href="#">MOODS AND GENRES</a>
+        <a href="#">NEW RELEASES</a>
+        <a href="#">DISCOVER</a>
+      </div>
+      <MusicSection artistName="queen" sectionTitle="Rock Classics" />
+      <MusicSection artistName="Gazzelle" sectionTitle="Pop Culture" />
+      <MusicSection artistName="eminem" sectionTitle="HipHop" />
+    </Container>
+  </main>
+);
 
-  useEffect(() => {
-    fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=rock")
-      .then((resp) => resp.json())
-      .then((results) => setRock(results.data))
-      .catch((error) => console.log(error));
-
-    fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=pop")
-      .then((resp) => resp.json())
-      .then((results) => setPop(results.data))
-      .catch((error) => console.log(error));
-
-    fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=hiphop")
-      .then((resp) => resp.json())
-      .then((results) => setHipHop(results.data))
-      .catch((error) => console.log(error));
-  }, []);
-  return (
-    <main className="mainPage">
-      <Container fluid>
-        <Row>
-          <Col>
-            <div className="mainLinks d-flex">
-              <a href="#">TRENDING</a>
-              <a href="#">PODCAST</a>
-              <a href="#">MOODS AND GENRES</a>
-              <a href="#">NEW RELEASES</a>
-              <a href="#">DISCOVER</a>
-            </div>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <div id="rock">
-              <h2>
-                <strong>Rock Classics</strong>
-              </h2>
-              <Row xs={1} sm={2} lg={3} xl={4} className="imgLinks py-3" id="rockSection">
-                {rock.slice(0, 4).map((song) => (
-                  <Col key={song.id}>
-                    <AlbumCard song={song} />
-                  </Col>
-                ))}
-              </Row>
-            </div>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <div id="pop">
-              <h2>
-                <strong>Pop Culture</strong>
-              </h2>
-              <Row xs={1} sm={2} lg={3} xl={4} className="imgLinks py-3" id="popSection">
-                {pop.slice(0, 4).map((song) => (
-                  <Col key={song.id}>
-                    <AlbumCard song={song} />
-                  </Col>
-                ))}
-              </Row>
-            </div>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <div id="hiphop">
-              <h2>
-                <strong>HipHop</strong>
-              </h2>
-              <Row xs={1} sm={2} lg={3} xl={4} className="imgLinks py-3" id="hipHopSection">
-                {hiphop.slice(0, 4).map((song) => (
-                  <Col key={song.id}>
-                    <AlbumCard song={song} />
-                  </Col>
-                ))}
-              </Row>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    </main>
-  );
-};
 export default MainSection;
