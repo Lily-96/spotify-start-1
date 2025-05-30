fbd0efed59d1b716966d324637f54d8b13dbbc6d
Lily-96 <LILIYASAVITSKA@GMAIL.COM>
2025-05-30 17:42:21 +0200

    fatta la selezione 🤯

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -6,37 +6,34 @@ const Player = () => {
   return (
     <Container fluid className="fixed-bottom bg-container pt-1">
       <Row className="h-100">
-        <Col lg={{ span: 10, offset: 2 }}>
-          <Row className="h-100 flex-column justify-content-center align-items-center">
-            {songSelected && (
-              <div className="d-inline-block align-middle mb-2">
-                <Image src={songSelected.album.cover_medium} width="70" className="mb-2" />
-                <div>
-                  <strong>{songSelected.title}</strong>
-                </div>
-              </div>
-            )}
-            <Col xs={6} md={4}>
-              <div className="d-flex playerControls justify-content-between">
-                <a href="#">
-                  <img src="assets/playerbuttons/shuffle.png" alt="shuffle" />
-                </a>
-                <a href="#">
-                  <img src="assets/playerbuttons/prev.png" alt="prev" />
-                </a>
-                <a href="#">
-                  <img src="assets/playerbuttons/play.png" alt="play" />
-                </a>
-                <a href="#">
-                  <img src="assets/playerbuttons/next.png" alt="next" />
-                </a>
-                <a href="#">
-                  <img src="assets/playerbuttons/repeat.png" alt="repeat" />
-                </a>
-              </div>
-              <ProgressBar now={40} className="mt-3 progress" />
-            </Col>
-          </Row>
+        <Col md={3} className="d-none d-md-block"></Col>
+        <Col xs={12} md={4} className="d-flex align-items-center">
+          {songSelected && (
+            <div className="d-inline-block align-middle mb-2">
+              <Image src={songSelected.album.cover_medium} width="60" />
+              <div style={{ color: "white" }}>{songSelected.title}</div>
+            </div>
+          )}
+        </Col>
+        <Col xs={6} md={4}>
+          <div className="d-flex playerControls justify-content-between">
+            <a href="#">
+              <img src="assets/playerbuttons/shuffle.png" alt="shuffle" />
+            </a>
+            <a href="#">
+              <img src="assets/playerbuttons/prev.png" alt="prev" />
+            </a>
+            <a href="#">
+              <img src="assets/playerbuttons/play.png" alt="play" />
+            </a>
+            <a href="#">
+              <img src="assets/playerbuttons/next.png" alt="next" />
+            </a>
+            <a href="#">
+              <img src="assets/playerbuttons/repeat.png" alt="repeat" />
+            </a>
+          </div>
+          <ProgressBar now={40} className="mt-3 progress" />
         </Col>
       </Row>
     </Container>
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -8,25 +8,24 @@ const SideBar = () => {
         <Navbar.Brand href="index.html" className="">
           <img src="../assets/logo/logo.png" alt="Spotify Logo" width="131" height="40" />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarNavAltMarkup" />
-        <Navbar.Collapse id="navbarNavAltMarkup">
-          <Nav className="flex-column w-100 ">
-            <Nav.Link href="#" className="d-flex align-items-center mx-2">
-              <i className="bi bi-house-door-fill"></i>&nbsp; Home
-            </Nav.Link>
-            <Nav.Link href="#" className="d-flex align-items-center mx-2">
-              <i className="bi bi-book-fill"></i>&nbsp; Your Library
-            </Nav.Link>
-            <Form className="mt-1 w-100  d-flex justify-content-start mx-2">
-              <InputGroup className="w-75">
-                <FormControl type="text" placeholder="Search" aria-label="Search" />
-                <Button variant="outline-secondary" size="sm">
-                  GO
-                </Button>
-              </InputGroup>
-            </Form>
-          </Nav>
-        </Navbar.Collapse>
+
+        <Nav className="flex-column w-100 ">
+          <Nav.Link href="#" className="d-flex align-items-center mx-2">
+            <i className="bi bi-house-door-fill"></i>&nbsp; Home
+          </Nav.Link>
+          <Nav.Link href="#" className="d-flex align-items-center mx-2">
+            <i className="bi bi-book-fill"></i>&nbsp; Your Library
+          </Nav.Link>
+          <Form className="mt-1 w-100  d-flex justify-content-start mx-2">
+            <InputGroup className="w-75">
+              <FormControl type="text" placeholder="Search" aria-label="Search" />
+              <Button variant="outline-secondary" size="sm">
+                GO
+              </Button>
+            </InputGroup>
+          </Form>
+        </Nav>
+
         <div className="nav-btn mt-auto w-100 d-flex flex-column align-items-center">
           <Button className="signup-btn mb-2" type="button">
             Sign Up
